2174863f75022525a2271bbcbbf5ef9c70837184
José I. Aliaga <aliaga@uji.es>
2017-07-17 16:23:22 +0100

    Mainly, including changes related to the testing files

---
+++ benchmark/testing.sh
@@ -0,0 +1,20 @@
+N_min=32 ; N_max=64; N_mlt=2;
+N_min=1 ; N_max=1000000000; N_mlt=10;
+N_min=1 ; N_max=1048576; N_mlt=2;
+N_min=1 ; N_max=67108864; N_mlt=2;
+N_min=1 ; N_max=16777216; N_mlt=2;
+N=$N_min
+while [ $N -le $N_max ] ; do
+  echo $N;
+  echo $N >> res.txt
+  ../build/tests/paper_clblas_test $N > clblas.txt
+  ../build/tests/paper_blas1_test  $N | cut -d, -f3- > syclblas.txt
+  cat syclblas.txt | cut -d, -f3- > aux.txt
+#  ../build/tests/paper_blas1_test  $N > syclblas.txt
+#  cat syclblas.txt | cut -d, -f3- > aux.txt
+#  paste -d, clblas.txt aux.txt >> res.txt
+  paste -d, clblas.txt syclblas.txt >> res.txt
+#  cat clblas.txt syclblas.txt aux.txt
+  N=`expr $N \* $N_mlt` 
+done
+
